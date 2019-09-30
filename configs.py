6527bdfe5ff78458add7d1b8b0f67f3d5ce085bb
hsol-lee <48540262+hsol-lee@users.noreply.github.com>
2019-09-30 11:20:52 +0900

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,8 +14,12 @@ tf.app.flags.DEFINE_string('check_point_path', './data_out/check_point', 'check 
 tf.app.flags.DEFINE_integer('shuffle_seek', 1000, 'shuffle random seek') # 셔플 시드값
 tf.app.flags.DEFINE_integer('max_sequence_length', 20, 'max sequence length') # 시퀀스 길이
 tf.app.flags.DEFINE_integer('embedding_size', 256, 'embedding size') # 임베딩 크기
+tf.app.flags.DEFINE_float('teacher_forcing_rate', 0.7, 'teacher forcing rate')
 tf.app.flags.DEFINE_boolean('tokenize_as_morph', True, 'set morph tokenize') # 형태소에 따른 토크나이징 사용 유무
 tf.app.flags.DEFINE_boolean('embedding', True, 'Use Embedding flag') # 임베딩 유무 설정
 tf.app.flags.DEFINE_boolean('multilayer', True, 'Use Multi RNN Cell') # 멀티 RNN 유무
+tf.app.flags.DEFINE_boolean('attention', True, 'Use Attention') #  어텐션 사용 유무
+tf.app.flags.DEFINE_boolean('teacher_forcing', True, 'Use Teacher Forcing') # 학습시 디코더 인풋 정답 지원 유무
+
 # Define FLAGS
 DEFINES = tf.app.flags.FLAGS
